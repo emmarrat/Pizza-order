d6ed7ed507be300d206102bc0e654d220fca0a61
Emir Marat <emmarrat@gmail.com>
2023-01-04 23:24:44 +0100

    Added spinner for orders route

--- src/containers/Orders/Orders.tsx
+++ src/containers/Orders/Orders.tsx
@@ -1,13 +1,15 @@
 import React, {useEffect} from 'react';
 import {useAppDispatch, useAppSelector} from "../../app/hooks";
 import {fetchOrders, removeOrder} from "../../features/pizza/pizzaThunks";
-import {selectOrders} from "../../features/pizza/pizzaSlice";
+import {selectOrders, selectPizzaFetchLoading} from "../../features/pizza/pizzaSlice";
 import NavbarAdmin from "../../components/Navbar/NavbarAdmin";
 import {DELIVERY_PRICE} from "../../constants";
+import Spinner from "../../components/Spinner/Spinner";
 
 const Orders = () => {
   const dispatch = useAppDispatch();
   const ordersState = useAppSelector(selectOrders);
+  const loading = useAppSelector(selectPizzaFetchLoading);
 
   useEffect(() => {
     dispatch(fetchOrders());
@@ -18,25 +20,35 @@ const Orders = () => {
       await dispatch(fetchOrders());
   };
 
+  let content = (
+    <>
+    {ordersState.length === 0 ? <h2 className="text-center">Orders list is empty</h2> : ordersState.map((order) => (
+        <div key={Math.random()} onClick={() =>deleteOrder(order.id)} className="card mb-2 p-2 w-50">
+          {order.order.map(o => (
+            <div key={Math.random()} className="d-flex align-items-center justify-content-between">
+              <p className="mb-0"> {o.name} x {o.amount}</p>
+              <p className="mb-0"> {o.total} KGS</p>
+            </div>
+          ))}
+          <hr/>
+          <p className="mb-0">Delivery price: <strong>{DELIVERY_PRICE} KGS</strong> </p>
+          <p className="fw-bolder mb-0">Grand total for the order: {order.total} KGS</p>
+          <button className="btn btn-outline-success mt-2">Complete order</button>
+        </div>
+      ))}
+    </>
+  )
+
+  if(loading){
+    content = (<Spinner/>)
+  }
+
   return (
     <>
       <NavbarAdmin/>
       <div className="container d-flex flex-column align-items-center">
       <h2 className="text-center mb-5">Orders:</h2>
-        {ordersState.length === 0 ? <h2 className="text-center">Orders list is empty</h2> : ordersState.map((order) => (
-          <div key={Math.random()} onClick={() =>deleteOrder(order.id)} className="card mb-2 p-2 w-50">
-            {order.order.map(o => (
-              <div key={Math.random()} className="d-flex align-items-center justify-content-between">
-                <p className="mb-0"> {o.name} x {o.amount}</p>
-                <p className="mb-0"> {o.total} KGS</p>
-              </div>
-            ))}
-            <hr/>
-            <p className="mb-0">Delivery price: <strong>{DELIVERY_PRICE} KGS</strong> </p>
-            <p className="fw-bolder mb-0">Grand total for the order: {order.total} KGS</p>
-            <button className="btn btn-outline-success mt-2">Complete order</button>
-          </div>
-        ))}
+        {content}
       </div>
     </>
 
